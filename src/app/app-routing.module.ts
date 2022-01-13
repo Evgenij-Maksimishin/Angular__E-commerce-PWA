e4e5970b9b1e288e27177b3bee3a9c14fb158abd
Yevhenii Maksimishin <evgenijmaksimisin@gmail.com>
2022-01-14 01:47:54 +0200

    Added main layout component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { CardPageComponent } from './card-page/card-page.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ProductPageComponent } from './product-page/product-page.component';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
 
 const routes: Routes = [
   {
-    path: '', component: MainPageComponent, children: [
+    path: '', component: MainLayoutComponent, children: [
       { path: '', redirectTo: '/', pathMatch: 'full' },
       { path: '', component: MainPageComponent },
       { path: 'product/:id', component: ProductPageComponent },
