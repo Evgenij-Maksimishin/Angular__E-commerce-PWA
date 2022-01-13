5887380df5880289c066191e557917f6a601b4f9
Yevhenii Maksimishin <evgenijmaksimisin@gmail.com>
2022-01-14 01:39:52 +0200

    Created routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardPageComponent } from './card-page/card-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ProductPageComponent } from './product-page/product-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainPageComponent, children: [
+      { path: '', redirectTo: '/', pathMatch: 'full' },
+      { path: '', component: MainPageComponent },
+      { path: 'product/:id', component: ProductPageComponent },
+      { path: 'card', component: CardPageComponent },
+
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
