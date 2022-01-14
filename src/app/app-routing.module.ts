5f527026770e0318be0e5f21efcf1af556845bc5
Yevhenii Maksimishin <evgenijmaksimisin@gmail.com>
2022-01-14 02:05:27 +0200

    Created path for admin role

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
 
+
 const routes: Routes = [
   {
     path: '', component: MainLayoutComponent, children: [
@@ -12,9 +13,11 @@ const routes: Routes = [
       { path: '', component: MainPageComponent },
       { path: 'product/:id', component: ProductPageComponent },
       { path: 'card', component: CardPageComponent },
-
-
     ]
+  },
+
+  {
+    path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
   }
 ];
 
