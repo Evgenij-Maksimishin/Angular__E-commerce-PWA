7cffbd8d3571e342805af1e0aff17afcb17bab28
Yevhenii Maksimishin <evgenijmaksimisin@gmail.com>
2022-01-15 00:08:11 +0200

    Created routing

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AdminLayoutComponent } from './shared/admin-layout/admin-layout.component';
+import { RouterModule } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AdminLayoutComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '', component: AdminLayoutComponent, children: [
+          { path: '', redirectTo: 'admin/login', pathMatch: 'full' },
+          { path: 'login', component: LoginPageComponent }
+        ]
+      }
+    ])
   ]
 })
 export class AdminModule { }
