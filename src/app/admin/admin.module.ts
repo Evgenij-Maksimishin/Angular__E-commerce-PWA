c842d53f23f9f129c9ba48d24f7945cc70a27d25
Yevhenii Maksimishin <evgenijmaksimisin@gmail.com>
2022-01-15 00:10:13 +0200

    Created add-page

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { AdminLayoutComponent } from './shared/admin-layout/admin-layout.component';
 import { RouterModule } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { AddPageComponent } from './add-page/add-page.component';
 
 
 
 @NgModule({
   declarations: [
-    AdminLayoutComponent
+    AdminLayoutComponent,
+    AddPageComponent
   ],
   imports: [
     CommonModule,
