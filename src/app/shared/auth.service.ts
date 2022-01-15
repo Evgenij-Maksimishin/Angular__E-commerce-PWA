7b0b3b94881a74d0706693ca76fbf763b912453b
Yevhenii Maksimishin <evgenijmaksimisin@gmail.com>
2022-01-16 01:13:13 +0200

    Added API Key

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  login (User) {
+    this.http.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]', User)
+  }
 }
